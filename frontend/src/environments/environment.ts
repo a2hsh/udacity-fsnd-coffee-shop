5d10e50e84918a986ca78dfee0ac3a30e78a815b
Abdulaziz Alshmasi <a2h.alshmasi@gmail.com>
2020-06-27 01:05:41 +0300

    Added the appropriate environment variables to the frontend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'fsnd-auth-a2h.us', // the auth0 domain prefix
+    audience: 'fsnd-auth', // the audience set for the auth0 app
+    clientId: 'PxOMYGbtt7nZU95vw3qUH71ueSzKf9TW', // the client id generated for the auth0 app
+    callbackURL: 'https://localhost:8100', // the base url of the running ionic application. 
   }
 };
